a3f74a20ac08fb5c88bcac31c6ad9e1a86e23110
Bewtaich <bewtaich@gmail.com>
2024-08-11 21:00:59 -0500

    Console prompts user when activated

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
+inquirer
+    .prompt([
+      {
+        name: 'Test',
+        message: 'How are you?',
+        type: 'input'
+      }
+    ])
+    .then((response) =>
+      console.log(response)
+    );
 // TODO: Create an array of questions for user input
-const questions = [];
+//Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+const questions = [
+   ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
 init();
+
